0a0fc6943d2cc8331ee5abe673eeb918d1c5d6f4
Santhosh Kumar L <lsanthoshkumar01@gmail.com>
2023-09-23 13:37:37 +0530

    commit1

---
+++ Shift/src/Pack/Shifting.java
@@ -0,0 +1,20 @@
+package Pack;
+
+public class Shifting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch='i';
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+		{
+			System.out.println(ch+"is a vowel");
+		}
+		else
+		{
+			System.out.println(ch+"is not a vowel");
+		}
+
+
+	}
+
+}
